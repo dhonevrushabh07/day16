9897c489a300eac3d4a6989e7b96f6441979ccd1
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-01-03 00:29:21 +0530

    day 16

---
+++ 1.js
@@ -0,0 +1,49 @@
+let obj = {
+  coord: {
+    lon: 72.8479,
+    lat: 19.0144,
+  },
+  weather: [
+    {
+      id: 721,
+      main: "Haze",
+      description: "haze",
+      icon: "50d",
+    },
+  ],
+  base: "stations",
+  main: {
+    temp: 23.99,
+    feels_like: 24.12,
+    temp_min: 23.94,
+    temp_max: 23.99,
+    pressure: 1017,
+    humidity: 64,
+  },
+  visibility: 2200,
+  wind: {
+    speed: 3.6,
+    deg: 100,
+  },
+  clouds: {
+    all: 20,
+  },
+  dt: 1640921311,
+  sys: {
+    type: 1,
+    id: 9052,
+    country: "IN",
+    sunrise: 1640914881,
+    sunset: 1640954486,
+  },
+  timezone: 19800,
+  id: 1275339,
+  name: "Mumbai",
+  cod: 200,
+};
+
+console.log(obj.main.temp);
+
+console.log(obj.dt);
+
+console.log(new Date(obj.dt * 1000)); // mult by 1000 to convert milisec to sec
